fc57f984f3d4cd957792cdec6ad36b2b4ed81d93
Wade Gooch <wadgooch@mail.mvnu.edu>
2016-11-17 01:24:18 -0500

    Adding twitter bootstrap; loaded bootstrap css in layouts file; tested bootstrap integration on homepage; added initialization date to homepage route.

--- solar-data-savings/routes/index.js
+++ solar-data-savings/routes/index.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 var models = require('../models');
 
-/* GET home page. */
+/* GET home page with locations and their initial data collection dates */
 router.get('/', function(req, res, next) {
   models.Location.findAll({
-    attributes: ['locationName']
+    attributes: ['locationName', 'initializationDate']
   }).then(function(locations) {
     res.render('index', { 
       title: 'Solar Data Savings',
