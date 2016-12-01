d9d494d14802c88268ea7409809028796f4e8869
Wade Gooch <wadgooch@mail.mvnu.edu>
2016-12-01 03:27:38 -0500

    Updated view with ALL BUT TIMEPICKERS; added in bootstrap-datepicker; added custom CSS; refactored answersHourly model instance method (the only one).

--- solar-data-savings/public/javascripts/savings.js
+++ solar-data-savings/public/javascripts/savings.js
@@ -1,4 +1,4 @@
-function getAnswersHourly() {
+function getAnswersHourlySavings() {
     var locationID = $('#locations option:selected').attr('value'),
         queryString = 'savings?locationID=' + locationID;
     var date = new Date(2016, 8, 5);
