3a2ddd2fd5455054c8190d97d52a1a36cb3bc233
nicisley <nicisley@mail.mvnu.edu>
2016-12-10 21:57:45 -0500

    Incomplete unit tests

--- solar-data-savings/tests/frontend/savings_tests.js
+++ solar-data-savings/tests/frontend/savings_tests.js
@@ -2,16 +2,35 @@ var assert = require('assert');
 var savings = require('../../public/javascripts/savings.js');
 
 describe('Savings Validations', function() {
-    describe('Start Date against Init Date', function() {
-        it('Should be valid if on init date', function() {
+    describe('Start Date before Init Date', function() {
+        it('Should be valid if start date on init date', function() {
             var initDate = new Date(),
                 startDate = new Date();
             assert.equal(savings.isInitDate(initDate, startDate), true);
         });
-        it('Should be invalid if before init date', function() {
+        it('Should be invalid if start date before init date', function() {
             var initDate = new Date(),
                 startDate = new Date();
             startDate.setDate(initDate.getDate()-1);
         });
     });
+
+    describe('End date before start date', function() {
+        it('Should be valid if end date on start date', function() {
+            var startDate = new Date(),
+                endDate = new Date();
+            assert.equal(savings.isValidDateRange1(startDate, endDate), true);
+        });
+        it('Should be invalid if end date before start date', function() {
+            var startDate = new Date(),
+                endDate = new Date();
+            endDate.setDate(startDate.getDate()-1);
+        });
+    });
+
+    describe('End date after current date', function() {
+        it('Should be valid if end date before current date', function() {
+            
+        })
+    })
 });
