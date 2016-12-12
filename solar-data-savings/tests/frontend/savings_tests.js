20375a69354ffcbe7a5b6b9fb16eb92416af091e
nicisley <nicisley@mail.mvnu.edu>
2016-12-11 22:57:45 -0500

    Finished tests for validation logic

--- solar-data-savings/tests/frontend/savings_tests.js
+++ solar-data-savings/tests/frontend/savings_tests.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 var savings = require('../../public/javascripts/savings.js');
 
 describe('Savings Validations', function() {
-    describe('Start Date before Init Date', function() {
+    describe('Start Date after Init Date', function() {
         it('Should be valid if start date on init date', function() {
             var initDate = new Date(),
                 startDate = new Date();
@@ -16,22 +16,30 @@ describe('Savings Validations', function() {
         });
     });
 
-    describe('End date before start date', function() {
-        it('Should be valid if end date on start date', function() {
+    describe('End date after start date', function() {
+        it('Should be valid if end date after start date', function() {
             var startDate = new Date(),
                 endDate = new Date();
+            endDate.setDate(startDate.getDate()+1);
             assert.equal(savings.isValidDateRange(startDate, endDate), true);
         });
-        it('Should be invalid if end date before start date', function() {
+        it('Should be invalid if end date on start date', function() {
             var startDate = new Date(),
                 endDate = new Date();
-            endDate.setDate(startDate.getDate()-1);
+            assert.equal(savings.isValidDateRange(startDate, endDate), false);
         });
     });
 
-    describe('End date after current date', function() {
-        it('Should be valid if end date before current date', function() {
-            
-        })
-    })
+    describe('End date before current date', function() {
+        it('Should be valid if end date equals current date', function() {
+            var endDate = new Date();
+            endDate.setHours(0);
+            assert.equal(savings.isValidEndDate(endDate), true);
+        });
+        it('Should be invalid if end date is greater than current date', function() {
+            var endDate = new Date();
+            endDate.setDate(endDate.getDate()+10);
+            assert.equal(savings.isValidEndDate(endDate), false);
+        });
+    });
 });
